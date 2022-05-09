e032602bf2bd3f76cd4c26cad510a682cb430a70
noamya-sh <noamyashany@gmail.com>
2022-05-09 14:58:06 +0300

    steal

--- Captain.cpp
+++ Captain.cpp
@@ -1,12 +1,18 @@
-//
-// Created by נעמיה on 28/04/2022.
-//
 #include "Game.hpp"
 #include "Captain.hpp"
 
 namespace coup {
     void Captain::steal(Player &player) {
-
+        if (player._coin>=2){
+            player._coin-=2;
+            this->_coin+=2;
+            this->last_act = STEAL2;
+        }
+        if (player._coin == 1) {
+            player._coin-=1;
+            this->_coin+=1;
+            this->last_act = STEAL1;
+        }
     }
 
     void Captain::block(Player &player) {
